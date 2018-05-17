af2f06d0736149f584915d85408a5d160bab21c5
harry <a@nc.om>
2018-05-16 18:34:21 -0700

    adding sbt

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "KafkaImplementation"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+
+libraryDependencies += "org.apache.kafka" %% "kafka" % "0.8.2.1"
+
+
---
+++ src/main/Java/KafkaImplementation.java
@@ -0,0 +1,21 @@
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+
+ class KafkaBroker1
+{
+
+    public static void main(String args[]){
+
+
+        System.out.println("hellod");
+
+        Properties kafkaConfig = new Properties();
+        kafkaConfig.put("","");
+
+    }
+}
